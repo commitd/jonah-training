1ba7e1c3a0c933e44bf1709118c22ca8e10a97d6
Chris Flatley <chris@committed.io>
2018-07-21 22:49:28 +0200

    Implement word count

--- Projects/jonah-server-wordcount/src/main/java/io/committed/training/jonah/wordcloud/DocumentWordCloud.java
+++ Projects/jonah-server-wordcount/src/main/java/io/committed/training/jonah/wordcloud/DocumentWordCloud.java
@@ -1,9 +1,11 @@
 package io.committed.training.jonah.wordcloud;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import com.google.common.collect.HashMultiset;
 import io.committed.invest.extensions.annotations.GraphQLService;
 import io.committed.ketos.common.data.BaleenDocument;
+import io.committed.training.jonah.wordcloud.utils.StopwordChecker;
 import io.leangen.graphql.annotations.GraphQLContext;
 import io.leangen.graphql.annotations.GraphQLQuery;
 
@@ -17,8 +19,45 @@ public class DocumentWordCloud {
   }
 
   private List<WordCount> countWords(String content, int minCount) {
-    // TODO: Implementation
-    return Collections.emptyList();
+    // Poor quality tokenisation
+    String[] split = content.split("\\s+");
+
+
+    // Count up the number of non-stopwords
+    HashMultiset<String> multiset = HashMultiset.create();
+    for (String w : split) {
+
+      String s = w.toLowerCase();
+
+      if (StopwordChecker.isNotStopWord(s)) {
+        multiset.add(s);
+      }
+    }
+
+    // Convert to a list of word counts
+    List<WordCount> list = new ArrayList<>();
+    for (String w : multiset.elementSet()) {
+
+      int count = multiset.count(w);
+      if (count >= minCount) {
+        list.add(new WordCount(w, count));
+      }
+    }
+
+    return list;
+
+    // Or via streams...
+    //
+    // return Arrays.stream(split)
+    // .filter(StopwordChecker::isNotStopWord)
+    // .collect(Collectors.groupingBy(w -> w, Collectors.counting()))
+    // .entrySet()
+    // .stream()
+    // .map(e -> new WordCount(e.getKey(), e.getValue()))
+    // .filter(c -> c.getCount() >= minCount)
+    // .sorted()
+    // .limit(size)
+    // .collect(Collectors.toList());
   }
 
 }
