2dbb7272b82e1f9707b3ea56a1ea4e4bc110cf69
Chris Flatley <chris@committed.io>
2018-07-21 22:49:18 +0200

    Create DocumentWordCloud service

--- Projects/jonah-server-wordcount/src/main/java/io/committed/training/jonah/wordcloud/DocumentWordCloud.java
+++ Projects/jonah-server-wordcount/src/main/java/io/committed/training/jonah/wordcloud/DocumentWordCloud.java
@@ -1,8 +1,24 @@
 package io.committed.training.jonah.wordcloud;
 
+import java.util.Collections;
+import java.util.List;
 import io.committed.invest.extensions.annotations.GraphQLService;
+import io.committed.ketos.common.data.BaleenDocument;
+import io.leangen.graphql.annotations.GraphQLContext;
+import io.leangen.graphql.annotations.GraphQLQuery;
 
 @GraphQLService
 public class DocumentWordCloud {
+
+  @GraphQLQuery(name = "words", description = "Count unique words")
+  public List<WordCount> getWords(@GraphQLContext final BaleenDocument document) {
+    return countWords(document.getContent(), 2);
+
+  }
+
+  private List<WordCount> countWords(String content, int minCount) {
+    // TODO: Implementation
+    return Collections.emptyList();
+  }
 
 }
